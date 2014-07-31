eb11184d1cb97afc24f5ea6e9adebb2300936ead
Shunit Ben-Tzvi <shunitbt@wix.com>
2014-07-31 17:46:57 +0300

    payment thanks

--- sites/all/themes/arava_center/template.php
+++ sites/all/themes/arava_center/template.php
@@ -1,1 +1,7 @@
 <?php
+
+function arava_center_preprocess_page(&$vars) {
+  if (arg(0) == 'registration' && arg(1) == 'thanks') {
+    $vars['theme_hook_suggestions'][] = 'page__blank';
+  }
+}
---
+++ sites/all/themes/arava_center/templates/page--blank.tpl.php
@@ -0,0 +1,21 @@
+<!-- #page -->
+<div id="page" class="clearfix">
+    <!-- #main-content -->
+    <div id="main-content">
+        <div class="container">
+            <div class="row">
+                <section>
+                    <!-- #main -->
+                    <div id="main" class="clearfix">
+                        <?php print render($page['content']); ?>
+                    </div>
+                    <!-- EOF:#main -->
+                </section>
+
+            </div>
+
+        </div>
+    </div>
+    <!-- EOF:#main-content -->
+</div>
+<!-- EOF: #page -->
