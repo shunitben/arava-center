96896276f7fd25eea519177a4dd7a4362c5e6f8e
Shunit Ben-Tzvi <shunitbt@wix.com>
2014-08-07 12:57:36 +0300

    registration css

--- sites/all/themes/arava_center/template.php
+++ sites/all/themes/arava_center/template.php
@@ -1,7 +1,15 @@
 <?php
 
+function arava_center_preprocess_html(&$vars) {
+  global $language;
+  $vars['classes_array'][] = $language->dir;
+}
+
 function arava_center_preprocess_page(&$vars) {
   if (arg(0) == 'registration' && arg(1) == 'thanks') {
     $vars['theme_hook_suggestions'][] = 'page__blank';
+  }
+  if (arg(0) == 'registration') {
+    drupal_add_css(path_to_theme() . '/css/registration.css');
   }
 }
