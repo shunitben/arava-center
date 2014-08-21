74328016876a3e0dfb5c35517905b4315529728c
Shunit Ben-Tzvi <shunitbt@wix.com>
2014-08-21 17:24:28 +0300

    course collision validation

--- sites/all/modules/custom/arava_registration/js/arava_registration.js
+++ sites/all/modules/custom/arava_registration/js/arava_registration.js
@@ -25,7 +25,30 @@
                 Drupal.behaviors.arava_registration.saveAndCloseTakanon(name, takanon, $(this));
             });
 
+            // time tables
             Drupal.behaviors.arava_registration.timetableNavigation();
+
+            // course validation - collisions
+            Drupal.behaviors.arava_registration.collectSelectedCourses();
+            $('.view-course-selection input[type="checkbox"]').click(function(e) {
+                if (!$(this).is(':checked')) {
+                    var times = $(this).parent().siblings('.views-row').find('.lesson-times').text();
+                    Drupal.behaviors.arava_registration.removeFromSelectedCourses(times);
+                }
+                else {
+                    var times = $(this).parent().siblings('.views-row').find('.lesson-times').text();
+                    var valid = Drupal.behaviors.arava_registration.validateCoursesCollision(times);
+                    if (valid !== false) {
+                        e.preventDefault();
+                        var error = '<div>' + Drupal.t('You cannot choose this course - it clashes with ') + valid.old_course + '</div>';
+                        $(error).dialog({
+                           height: 200,
+                           width: 300,
+                           modal: true
+                        });
+                    }
+                }
+            });
         },
 
         showTakanon: function (id, link) {
@@ -101,6 +124,51 @@
                     currentMonth = currentMonth - 1;
                 }
             });
+        },
+
+        collectSelectedCourses: function() {
+            var allTimes = [];
+            $('.view-course-selection input[type="checkbox"]').filter(':checked').each(function(i){
+                var courseTimes = $(this).parent().siblings('.views-row').find('.lesson-times').text();
+                courseTimes = JSON.parse(courseTimes);
+                allTimes = allTimes.concat(courseTimes);
+            });
+            Drupal.behaviors.arava_registration.selectedCoursesTimes = allTimes;
+        },
+
+        validateCoursesCollision: function(courseTimes) {
+            var allTimes = Drupal.behaviors.arava_registration.selectedCoursesTimes;
+            courseTimes = JSON.parse(courseTimes);
+
+            var i,j;
+            for (i=0; i < courseTimes.length; i++) {
+                for (j=0; j < allTimes.length; j++) {
+                    // check collision
+                    if ((courseTimes[i]['start'] >= allTimes[j]['start'] && courseTimes[i]['start'] < allTimes[j]['end'])
+                        || (courseTimes[i]['end'] > allTimes[j]['start'] && courseTimes[i]['end'] <= allTimes[j]['end'])) {
+                        return {new_course: courseTimes[i]['course_name'], old_course: allTimes[j]['course_name']};
+                    }
+                }
+                // add to allTimes
+                allTimes.push(courseTimes[i]);
+            }
+            // if we haven't returned yet, let's update allTimes
+            Drupal.behaviors.arava_registration.selectedCoursesTimes = allTimes;
+            return false;
+        },
+
+        removeFromSelectedCourses: function(courseTimes) {
+            var allTimes = Drupal.behaviors.arava_registration.selectedCoursesTimes;
+            courseTimes = JSON.parse(courseTimes);
+            var removeKey = courseTimes[0]['course_id'],
+                rebuildAllTimes = [];
+
+            for (var key in allTimes) {
+                if (allTimes[key]['course_id'] != removeKey) {
+                    rebuildAllTimes.push(allTimes[key]);
+                }
+            }
+            Drupal.behaviors.arava_registration.selectedCoursesTimes = rebuildAllTimes;
         }
 
     }
