f8f9933161a3f97ac047f23fa8adebf722a6bb92
Shunit Ben-Tzvi <shunitbt@wix.com>
2015-01-30 15:26:13 +0200

    doing assignments + list on course page

---
+++ sites/all/modules/custom/arava_assignments/js/arava_assignments.js
@@ -0,0 +1,14 @@
+(function (Drupal, $) {
+    'use strict';
+
+    Drupal.behaviors.arava_assignments = {
+        attach: function (context) {
+// working very badly!
+//            $('#edit-checker').chosen();
+
+        }
+
+
+
+    }
+}(window.Drupal, window.jQuery));
