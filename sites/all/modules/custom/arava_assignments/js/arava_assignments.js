a690f10c96103d83b682bb7c1985f33eb212296b
Shunit Ben-Tzvi <shunitbt@wix.com>
2015-02-14 13:55:55 +0200

    finshed requirements - confirmation

--- sites/all/modules/custom/arava_assignments/js/arava_assignments.js
+++ sites/all/modules/custom/arava_assignments/js/arava_assignments.js
@@ -6,6 +6,17 @@
 // working very badly!
 //            $('#edit-checker').chosen();
 
+            $('#arava-assignments-do-assignment-form #edit-submit-done').click(function(e) {
+                var confirmationText = Drupal.t("By clicking this button, you are saying that you completed all the " +
+                                                "assignments needed to enter the next lesson.\n" +
+                                                "This includes answering the questions, and for homework assignments, " +
+                                                "it also includes meditations and sometimes debates.\n\n" +
+                                                "Do you confirm that this is what you meant to do?");
+                if (!window.confirm(confirmationText)) {
+                    e.preventDefault();
+                }
+            })
+
         }
 
 
