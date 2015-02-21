4cac592b5a90bedb6ed6955eeb2c95ab06b7ef72
Shunit Ben-Tzvi <shunitbt@wix.com>
2015-02-21 11:43:38 +0200

    animals - fixes

--- sites/all/modules/custom/arava_center/js/arava_center.js
+++ sites/all/modules/custom/arava_center/js/arava_center.js
@@ -3,21 +3,18 @@
 
     Drupal.behaviors.arava_center = {
         attach: function (context) {
-//            $('.view-animals .view-content').accordion({
-//                collapsible: true,
-//                heightStyle: "content",
-//                active:false
-//            });
 
-            $('.view-animals .view-grouping-content').hide();
-            $('.view-animals .view-grouping-header').prepend('<span class="toggle plus">+</span> ');
+            if (context == document) {
+                $('.view-animals .view-grouping-content').hide();
+                $('.view-animals .view-grouping-header').prepend('<span class="toggle plus">+</span> ');
 
-            $('.view-animals .view-grouping-header').click(function(){
-                var sibling = $(this).siblings('.view-grouping-content');
-                sibling.toggle();
+                $('.view-animals .view-grouping-header').click(function(){
+                    var sibling = $(this).siblings('.view-grouping-content');
+                    sibling.toggle();
 
-                $('.view-animals .view-grouping-content').not(sibling).hide();
-            });
+                    $('.view-animals .view-grouping-content').not(sibling).hide();
+                });
+            }
         }
 
     }
